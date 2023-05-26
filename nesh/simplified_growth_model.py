1e72c997a943f08bb4422197d4e56c545c155540
sneshyba <nesh@pugetsound.edu>
2023-05-26 07:53:52 -0700

    Update simplified_growth_model.py

--- nesh/simplified_growth_model.py
+++ nesh/simplified_growth_model.py
@@ -10,16 +10,17 @@ Created on Tue May 23 17:43:50 2023
 import numpy as np
 from matplotlib import pyplot as plt
 
+
 # Parameters of the system
 l = 25
 npts = 500
-midpt = np.round(npts/2).astype(int); print(midpt)
-x = np.linspace(-l,l,npts)
+midpt = np.round(npts / 2).astype(int)
+print(midpt)
+x = np.linspace(-l, l, npts)
 
 # Let's look at a steady-state solution
 N_l = 11
-k = N_l/l**2
-f_ss = k*x**2
+k = N_l / l**2
+f_ss = k * x**2
 plt.figure()
-plt.plot(x,f_ss)
-
+plt.plot(x, f_ss)
