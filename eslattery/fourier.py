0d85aeccf733a42c67572bbf6eb944ed4bcb67fe
Ella Slattery <eslattery@pugetsound.edu>
2023-06-26 15:19:10 -0700

    beginnning work on fourier transform

--- eslattery/fourier.py
+++ eslattery/fourier.py
@@ -14,7 +14,12 @@ import matplotlib.pyplot as plt
 
 def fftnorm(u_full):
     """Computes normalized FFT (such that FFT and IFFT are symmetrically normalized)
-    
+    ### rfft excludes redundant outputs; complex conjugates are left out so every bin only has the positive frequencies
+    ### norm='forward' arg normalizes transform by 1/n, inverse transform is unscaled
+    ### TODO: qs
+    ###     does this mean the inverse transform is natrually scaled back to map to original untransformed scale??
+    ###     is this better than (eg MZKdV.py) mult by 1/N??
+
     Parameters
     ----------
     u_full : 1D Numpy Array (N,)
@@ -32,7 +37,8 @@ def fftnorm(u_full):
 
 def ifftnorm(u_full):
     """Computes normalized IFFT (such that FFT and IFFT are symmetrically normalized)
-    
+    ### irfft is inverse of rfft, norm is forward for symmetric normalizations
+
     Parameters
     ----------
     u_full : 1D Numpy Array (N,)
@@ -55,20 +61,24 @@ def convolution(nT,nu_kin,sigmaM,Nstar):
     
     Computed in real space and then converted back
     to Fourier space.
-    
+
+    ### sigmaM is eqn 6 in paper, ~~delta kinda~~ but entirely dependent on Nqll
+    ###
+
     Parameters
     ----------
     nT : 1D Numpy Array (N,)
-        Total water layers
+        Total water layers, in k space
         
     nu_kin : TBD
-        TBD
+        TODO--
+        Deposition rate? scalar value?
         
-    sigmastep : TBD
-        TBD
+    sigmaM : TBD
+        Microscopic supersaturation, dependent on position through m dependence on Nqll, in real space
         
     Nstar : TBD
-        TBD
+        Parameterizes variation about the mean (simply a best fit parameter??)
 
     Returns
     -------
@@ -83,7 +93,7 @@ def convolution(nT,nu_kin,sigmaM,Nstar):
 def nTotRHS(nQLL,nu_kin,sigmaM,k,D):
     """Computes RHS of the ODE for the positive modes of Ntot
     
-    ##TODO: why factor of 2pi??
+    ##TODO: why factor of 2pi?? how are we normalizing here??
     dnk/dt = -k^2 D nkQLL + 2 pi FFT(sigma_m) nu_kin
     
     
@@ -93,13 +103,13 @@ def nTotRHS(nQLL,nu_kin,sigmaM,k,D):
         Positive modes of state vector for quasi-liquid layers
         
     nu_kin : TBD
-        TBD
+        Deposition rate? scalar value?
         
-    sigmastep_FFT : TBD
-        TBD
+    sigmaM : TBD
+        Microscopic supersaturation, dependent on position through m dependence on Nqll, in real space
         
     k : 1D Numpy Array (N,)
-        Vector of wavenumbers
+        Vector of (nonredundant?) wavenumbers
         
     D : float
         Diffusion coefficient
@@ -116,6 +126,7 @@ def nTotRHS(nQLL,nu_kin,sigmaM,k,D):
 def nQLLRHS(nTot,nQLL,nu_kin,depRate,k,D,Nstar,N):
     """Computes RHS of the ODE for the positive modes of Ntot
     
+    ## N is unused.... why is it here?
     ##TODO: i dont see where the 2pi in dn0 comes from??
     dn0/dt = 2 * pi * sigma_m * nu_kin
     dnk/dt = -k^2 D nkQLL
@@ -150,6 +161,8 @@ def nQLLRHS(nTot,nQLL,nu_kin,depRate,k,D,Nstar,N):
         Rate of change of positive modes of nTot
     """
     
+    ## convolution computed: 
+    ## 2 * np.pi * Nstar * nu_kin * fftnorm(depRate * np.cos(2*np.pi * ifftnorm(nTot)))
     convo = convolution(nTot,nu_kin,depRate, Nstar)
     dnQLL = -k**2 * D * nQLL + convo
     return dnQLL
