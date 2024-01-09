87fbcd6ab642832e4fa7e5b28a337a4c4c2f2d2a
sneshyba <nesh@pugetsound.edu>
2024-01-09 12:14:59 -0800

    More exploration of parameters

--- nesh/model development - DeltaNQLL/diffusionstuff10.py
+++ nesh/model development - DeltaNQLL/diffusionstuff10.py
@@ -6,6 +6,13 @@ Created on Tue Jul 14 15:01:47 2015
 import numpy as np
 import copy
 from numba import njit, float64, int32, types
+from scipy.integrate import solve_ivp
+
+def getsigma_m(NQLL0,Nbar,Nstar,sigmaI,sigma0):
+    twopi = 2*np.pi
+    m = (NQLL0 - (Nbar - Nstar))/(2*Nstar)
+    sigma_m = (sigmaI - m * sigma0)/(1+m*sigma0)
+    return sigma_m
 
 @njit
 def getNQLL(Ntot,Nstar,Nbar):
@@ -78,6 +85,7 @@ def f1d_solve_ivp(t, y, scalar_params, sigmaI):
     # Ntot deposition
     twopi = 2*np.pi
     m = (NQLL0 - (Nbar - Nstar))/(2*Nstar)
+#     sigma_m = (sigmaI - m * sigma0)/(1+m*sigma0)*0.997-0.0002
     sigma_m = (sigmaI - m * sigma0)/(1+m*sigma0)
     depsurf = nu_kin_mlyperus * sigma_m
     dNtot_dt = depsurf
@@ -101,6 +109,78 @@ def f1d_solve_ivp(t, y, scalar_params, sigmaI):
     derivs[l:] = dNtot_dt
 
     return derivs
+
+
+def run_f0d(NQLL_init_0D,Ntot_init_0D,times,params,odemethod):
+    # Call the ODE solver
+    ylast = np.array([NQLL_init_0D,Ntot_init_0D])
+    ykeep_0D = [ylast]
+    lastprogress = 0
+
+    nt = len(times)
+    for i in range(0,nt-1):
+
+        # Specify the time interval of this step
+        tinterval = [times[i],times[i+1]]
+        
+        # Integrate up to next time step
+        sol = solve_ivp(f0d_solve_ivp, tinterval, ylast, dense_output=True, args=(params,),rtol=1e-12,method=odemethod)
+        ylast = sol.y[:,-1]
+
+        # Stuff into keeper arrays
+        ykeep_0D.append(ylast)
+        
+        # Progress reporting
+        progress = int(i/nt*100)
+        if np.mod(progress,10) == 0:
+            if progress > lastprogress:
+                print(progress,'% done')
+                lastprogress = progress
+
+    print('100% done')
+    ykeep_0D = np.array(ykeep_0D, np.float64)
+    NQLLkeep_0D = ykeep_0D[:,0]
+    Ntotkeep_0D = ykeep_0D[:,1]
+
+    return Ntotkeep_0D, NQLLkeep_0D 
+
+
+def run_f1d(NQLL_init_1D,Ntot_init_1D,times,scalar_params,sigmaI,odemethod):
+    # Call the ODE solver
+    nt = len(times)
+    nx = len(NQLL_init_1D)
+    ylast = np.array([NQLL_init_1D,Ntot_init_1D])
+    ylast = np.reshape(ylast,2*nx)
+    ykeep_1D = [ylast]
+    lastprogress = 0
+
+
+    for i in range(0,nt-1):
+
+        # Specify the time interval of this step
+        tinterval = [times[i],times[i+1]]
+        
+        # Integrate up to next time step
+        sol = solve_ivp(f1d_solve_ivp, tinterval, ylast, args=(scalar_params,sigmaI),rtol=1e-12,method=odemethod)
+        ylast = sol.y[:,-1]
+        
+        # Stuff into keeper arrays
+        ykeep_1D.append(ylast)
+        
+        # Progress reporting
+        progress = int(i/nt*100)
+        if np.mod(progress,10) == 0:
+            if progress > lastprogress:
+                print(progress,'% done')
+                lastprogress = progress
+
+    print('100% done')
+    ykeep_1D = np.array(ykeep_1D, np.float64)
+    ykeep_1Darr = np.array(ykeep_1D, np.float64)
+    ykeep_1Darr_reshaped = np.reshape(ykeep_1Darr,(nt,2,nx))
+    Ntotkeep_1D = ykeep_1Darr_reshaped[:,1,:]
+    NQLLkeep_1D = ykeep_1Darr_reshaped[:,0,:]
+    return Ntotkeep_1D, NQLLkeep_1D
 
 
 # def f0d_solve_ivp_1var(t, y, myparams):
